7434e35a52bf397e4eab029a2b3d1ca60d9c45f5
Yann Christophe Maertens <iroshinidako@gmail.com>
2020-11-17 14:58:48 +0100

    Initial Commit

---
+++ Summon 'Em all !/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Summon 'Em all !
+//
+//  Created by Yann Christophe Maertens on 17/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Summon 'Em all !/Summon__Em_all__App.swift
@@ -0,0 +1,17 @@
+//
+//  Summon__Em_all__App.swift
+//  Summon 'Em all !
+//
+//  Created by Yann Christophe Maertens on 17/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Summon__Em_all__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
